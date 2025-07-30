e43be407c00238c0b92829f3b44f36199f3957ef
Camillem62 <camillem62.pro@gmail.com>
2025-07-31 01:09:33 +0200

    charac

--- Scripts/CharacterScript.gd
+++ Scripts/CharacterScript.gd
@@ -1,6 +1,18 @@
 extends CharacterBody2D
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var go = false 
 
 func _process(delta: float) -> void:
 	if Global.advance == true:
+		go = true
+		Global.isReadyToAdvance.set("characterReady", false)
+		
+	if go == true:
 		print("J'avance!")
+		animated_sprite.play('move_right')
+		for i in 4:
+			position.x += 4
+		animated_sprite.play("right")
+		
+		
 		Global.isReadyToAdvance.set("characterReady", true)
