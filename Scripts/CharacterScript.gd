8a2054cc0af3c1257b8722f90bb418acc2a062a5
Namu <49788407+namuuu@users.noreply.github.com>
2025-07-31 01:20:21 +0200

    fix: good movement 🐶

--- Scripts/CharacterScript.gd
+++ Scripts/CharacterScript.gd
@@ -1,18 +1,21 @@
 extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 var go = false 
+var goal
+var SPEED = 50
 
 func _process(delta: float) -> void:
 	if Global.advance == true:
 		go = true
 		Global.isReadyToAdvance.set("characterReady", false)
+		goal = Vector2(position.x + 16, position.y + 0)
 		
 	if go == true:
-		print("J'avance!")
+		#print("J'avance!")
 		animated_sprite.play('move_right')
-		for i in 4:
-			position.x += 4
-		animated_sprite.play("right")
+		position = position.move_toward(goal, SPEED * delta)
 		
-		
-		Global.isReadyToAdvance.set("characterReady", true)
+		if(position.x == goal.x && position.y == goal.y):
+			go = false
+			animated_sprite.play("right")
+			Global.isReadyToAdvance.set("characterReady", true)
