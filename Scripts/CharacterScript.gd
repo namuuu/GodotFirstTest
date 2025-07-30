4c8c315d4c1a262c5512480178040fac9c8a8830
Namu <49788407+namuuu@users.noreply.github.com>
2025-07-30 23:53:49 +0200

    fix: Timer force stop

--- Scripts/CharacterScript.gd
+++ Scripts/CharacterScript.gd
@@ -3,4 +3,4 @@ extends CharacterBody2D
 func _process(delta: float) -> void:
 	if Global.advance == true:
 		print("J'avance!")
-		Global.isReadyToAdvance.set("characterReady", false)
+		Global.isReadyToAdvance.set("characterReady", true)
